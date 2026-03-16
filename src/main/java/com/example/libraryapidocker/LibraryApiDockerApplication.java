466fcc47a438d382b45c1c4896bf41ef63e0033d
therealbladezxi-glitch <therealbladezxi@gmail.com>
2026-03-16 21:55:23 +0100

    init commit

---
+++ src/main/java/com/example/libraryapidocker/LibraryApiDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryapidocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApiDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraryapidocker/LibraryApiDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryapidocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
