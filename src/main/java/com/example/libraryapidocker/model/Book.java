342ccccc09c8a4aece703292cb7596106c51322c
therealbladezxi-glitch <therealbladezxi@gmail.com>
2026-03-16 22:47:27 +0100

    book model

--- src/main/java/com/example/libraryapidocker/model/Book.java
+++ src/main/java/com/example/libraryapidocker/model/Book.java
@@ -1,5 +1,6 @@
 package com.example.libraryapidocker.model;
 
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,36 +9,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
-@Table(name = "users")
+@Table(name = "books")
+@Schema(description = "Book entity")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "User entity")
-public class User {
+public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID isbn;
 
     @Column(nullable = false)
-    private String name;
-
+    private String title;
     @Column(nullable = false)
-    private String surname;
-
+    private String author;
+    @Column(nullable = true)
+    private LocalDate releaseYear;
     @Column(nullable = false)
-    private String mail;
-
-    @Column(nullable = false)
-    private String pass;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private UserType type;
-
-    private LocalDate memberShipDate;
-
+    private int copies;
 }
