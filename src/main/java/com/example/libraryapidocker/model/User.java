856ed727f23400a3df77abcf3b0b821ff6fd1a3a
therealbladezxi-glitch <therealbladezxi@gmail.com>
2026-03-16 22:24:06 +0100

    prova user model

---
+++ src/main/java/com/example/libraryapidocker/model/User.java
@@ -0,0 +1,38 @@
+package com.example.libraryapidocker.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "User entity")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String mail;
+
+    @Column(nullable = false)
+    private String pass;
+
+    @Column(nullable = false)
+    private UserType type;
+
+}
---
+++ src/main/java/com/example/libraryapidocker/model/UserType.java
@@ -0,0 +1,7 @@
+package com.example.libraryapidocker.model;
+
+public enum UserType {
+    SUPER,
+    ADMIN,
+    USER;
+}
